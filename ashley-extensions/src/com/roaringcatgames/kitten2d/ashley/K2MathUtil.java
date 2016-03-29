a80de31a7a0cc8e85ed59fb82ff99c9455af4b56
Barryrowe <barryrowe@gmail.com>
2016-03-29 19:56:21 -0400

    Calculate Random Between
    Provides a Calculate Random between a min/max function.

--- ashley-extensions/src/com/roaringcatgames/kitten2d/ashley/K2MathUtil.java
+++ ashley-extensions/src/com/roaringcatgames/kitten2d/ashley/K2MathUtil.java
@@ -1,11 +1,19 @@
 package com.roaringcatgames.kitten2d.ashley;
 
+import java.util.Random;
+
 /**
  * Created by barry on 3/5/16 @ 2:07 PM.
  */
 public class K2MathUtil {
 
+    private static Random r = new Random();
+
     public static double getSineYForTime(double period, double scale, double timePosition){
         return  Math.sin(timePosition*2*Math.PI/period)*(scale/2);
+    }
+
+    public static float getRandomInRange(float min, float max){
+        return (r.nextFloat()* (max-min)) + min;
     }
 }
